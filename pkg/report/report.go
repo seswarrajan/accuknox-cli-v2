69577de75946a15cb226bb392237f77aab856541
Swarit Pandey <swarit@accuknox.com>
2024-02-19 11:55:43 +0530

    chore(report): add timestamp to output files
    Signed-off-by: Swarit Pandey <swarit@accuknox.com>

--- pkg/report/report.go
+++ pkg/report/report.go
@@ -2,12 +2,20 @@ package report
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/accuknox/accuknox-cli-v2/pkg/common"
 	"github.com/accuknox/accuknox-cli-v2/pkg/summary"
 	"github.com/clarketm/json"
 	"github.com/kubearmor/kubearmor-client/k8s"
 )
+
+type outputPaths struct {
+	Level4NodesOutput string
+	PrMDOutput        string
+	DiffOutput        string
+	LatestSummaryPath string
+}
 
 func Report(client *k8s.Client, o *summary.Options) error {
 	if o.BaselineSummaryPath == "" {
@@ -35,22 +43,20 @@ func Report(client *k8s.Client, o *summary.Options) error {
 	tracker.FilterGraph(latestSummary.GetHash(), o)
 
 	// Determine output paths
-	predefinedPath := "knoxctl_out/reports/"
-	level4NodesOutput := predefinedPath + "level_4_nodes.json"
-	prMDOutput := predefinedPath + "pr_report.md"
-	diffOutput := predefinedPath + "diff.json"
-	if o.OutputTo != "" {
-		level4NodesOutput = o.OutputTo + "level_4_nodes.json"
-		prMDOutput = o.OutputTo + "pr_report.md"
-		diffOutput = o.OutputTo + "diff.json"
-	}
+	currentTime := time.Now().UTC().Format("20060102-150405")
+	outputPaths := generateOutputPaths(o.OutputTo, currentTime)
 
-	err = tracker.markdownPR(prMDOutput, latestSummary.GetHash())
+	err = writeLatestSummary(outputPaths.LatestSummaryPath, latestSummary)
 	if err != nil {
 		return err
 	}
 
-	err = tracker.writeDiffJSON(diffOutput, latestSummary.GetHash())
+	err = tracker.markdownPR(outputPaths.PrMDOutput, latestSummary.GetHash())
+	if err != nil {
+		return err
+	}
+
+	err = tracker.writeDiffJSON(outputPaths.DiffOutput, latestSummary.GetHash())
 	if err != nil {
 		return err
 	}
@@ -66,7 +72,7 @@ func Report(client *k8s.Client, o *summary.Options) error {
 	if o.Debug {
 		tracker.printDFSTraversal(latestSummary.GetHash())
 
-		err = tracker.writeLevel4NodesToJSONFile(level4NodesOutput, latestSummary.GetHash())
+		err = tracker.writeLevel4NodesToJSONFile(outputPaths.Level4NodesOutput, latestSummary.GetHash())
 		if err != nil {
 			return err
 		}
@@ -133,4 +139,33 @@ func setHashForResource(resources map[string]*summary.WorkloadEvents) error {
 		}
 	}
 	return nil
+}
+
+func writeLatestSummary(outpath string, workload *summary.Workload) error {
+	jsonData, err := json.MarshalIndent(workload, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	err = common.CleanAndWrite(outpath, jsonData)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Latest summary (in json) written to: %s\n", outpath)
+	return nil
+}
+
+func generateOutputPaths(outputDir, currentTime string) outputPaths {
+	basePath := "knoxctl_out/reports/"
+	if outputDir != "" {
+		basePath = outputDir
+	}
+
+	return outputPaths{
+		Level4NodesOutput: fmt.Sprintf("%slevel_4_nodes_%s.json", basePath, currentTime),
+		PrMDOutput:        fmt.Sprintf("%spr_report_%s.md", basePath, currentTime),
+		DiffOutput:        fmt.Sprintf("%sdiff_%s.json", basePath, currentTime),
+		LatestSummaryPath: fmt.Sprintf("%slatest_summary_%s.json", basePath, currentTime),
+	}
 }
