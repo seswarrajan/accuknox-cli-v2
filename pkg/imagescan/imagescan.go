74d929fcc87647ec7e0290ad4e3bb61ca7951bbf
Paveen Kumar <paveenkumar@accuknox.com>
2025-08-05 17:18:52 +0530

    feat(image-scan): Add options to discover and scan all images
    Signed-off-by: Paveen Kumar <paveenkumar@accuknox.com>

--- pkg/imagescan/imagescan.go
+++ pkg/imagescan/imagescan.go
@@ -6,22 +6,19 @@ import (
 	"os"
 	"syscall"
 
+	"github.com/accuknox/kubeshield/api/v1beta1"
 	kubesheildDiscovery "github.com/accuknox/kubeshield/pkg/discovery"
-	kubesheildConfig "github.com/accuknox/kubeshield/pkg/scanner/config"
+	httpclient "github.com/accuknox/kubeshield/pkg/scanner/httpClient"
 	kubesheildScanner "github.com/accuknox/kubeshield/pkg/scanner/scan"
 	"github.com/samber/lo"
 	"go.uber.org/zap"
 )
 
 // Discovers the running container images and scans the images using the specified tool
-func DiscoverAndScan(conf kubesheildConfig.Config, hostName, runtime string) error {
+func DiscoverAndScan(conf kubesheildScanner.ScanConfig, hostName, runtime string, onlyRunningContainers, onlyImages bool) error {
 	zapLogger, err := zap.NewProduction()
 	if err != nil {
 		return fmt.Errorf("failed to initialize logger")
-	}
-
-	if zapLogger != nil {
-		conf.Log = zapLogger.Sugar()
 	}
 
 	defer func() {
@@ -41,16 +38,20 @@ func DiscoverAndScan(conf kubesheildConfig.Config, hostName, runtime string) err
 		defer cleanupInstalledBinaryPath()
 	}
 
-	conf.Images = discoverImages(zapLogger.Sugar(), hostName, runtime)
+	conf.Images = discoverImages(hostName, runtime, onlyRunningContainers, onlyImages, zapLogger.Sugar())
 	if len(conf.Images) == 0 {
 		return fmt.Errorf("no images found for scanning")
 	}
 
 	// removes duplicate images
-	conf.Images = lo.Uniq(conf.Images)
+	conf.Images = lo.UniqBy(conf.Images, func(img v1beta1.Image) string {
+		return img.Name
+	})
+
 	for i := range conf.Images {
-		zapLogger.Info("Discovered Image", zap.String("Name", conf.Images[i].Name), zap.String("Runtime", conf.Images[i].Runtime))
+		zapLogger.Sugar().Infof("Image Name: %s | Runtime: %s", conf.Images[i].Name, conf.Images[i].Runtime)
 	}
+
 	zapLogger.Info("Images Discovered Successfully", zap.Int("Total number of images:", len(conf.Images)))
 
 	if hostName == "" {
@@ -58,10 +59,11 @@ func DiscoverAndScan(conf kubesheildConfig.Config, hostName, runtime string) err
 	}
 
 	// Additional fields added along with the scan results while calling artifact API
-	conf.ScanConfig.AdditionalData = map[string]any{"host_name": hostName}
-	conf.ScanConfig.ScanTool = "trivy" // Default scanning tool
+	conf.ArtifactConfig.AdditionalData = map[string]any{"host_name": hostName}
+	conf.ScanTool = "trivy" // Default scanning tool
 
 	imageScanner := kubesheildScanner.New(conf)
+	imageScanner.ScannerHttpClient = httpclient.New()
 
 	// Scans the provided images and sends the result back to saas through the artifact API
 	if err := imageScanner.Scan(); err != nil {
@@ -70,16 +72,16 @@ func DiscoverAndScan(conf kubesheildConfig.Config, hostName, runtime string) err
 
 	zapLogger.Info("Images Scanned Successfully",
 		zap.Int("Total Scanned Images", len(conf.Images)),
-		zap.String("Tool used for scanning", conf.ScanConfig.ScanTool))
+		zap.String("Tool used for scanning", conf.ScanTool))
 
 	return nil
 }
 
 // Lists the running containers for the provided runtime, if the runtime is empty it will use the default supported runtimes
-func discoverImages(logger *zap.SugaredLogger, hostName, runtime string) []kubesheildConfig.Image {
+func discoverImages(hostName, runtime string, onlyRunningContainers, imageOnly bool, logger *zap.SugaredLogger) []v1beta1.Image {
 	var (
 		runtimes = []string{"docker", "containerd", "cri-o", "nri"}
-		images   []kubesheildConfig.Image
+		images   []v1beta1.Image
 	)
 
 	if runtime != "" {
@@ -93,13 +95,25 @@ func discoverImages(logger *zap.SugaredLogger, hostName, runtime string) []kubes
 			logger.Errorf("Unable to detect runtime for %s", r)
 			continue
 		}
-		imageList := kubesheildDiscovery.ListRunningImages(detectedRuntime, criPath, kubesheildDiscovery.VM, logger)
-		for _, img := range imageList {
-			images = append(images, kubesheildConfig.Image{
-				Name:    img,
-				Runtime: detectedRuntime,
-			})
+
+		// If imageOnly flag is enabled, we only discover images; not containers
+		if imageOnly {
+			imageList, err := kubesheildDiscovery.ListImages(detectedRuntime, criPath, kubesheildDiscovery.VM)
+			if err != nil {
+				logger.Errorf("error while listing the images for runtime %s: %v", r, err)
+				continue
+			}
+			images = append(images, imageList...)
+			continue
 		}
+
+		// By default we fetch running containers, unless onlyRunningContainers is set to false
+		containerList, err := kubesheildDiscovery.ListContainers(detectedRuntime, criPath, kubesheildDiscovery.VM, onlyRunningContainers)
+		if err != nil {
+			logger.Errorf("error while listing the container images for runtime %s: %v", r, err)
+			continue
+		}
+		images = append(images, containerList...)
 	}
 	return images
 }
