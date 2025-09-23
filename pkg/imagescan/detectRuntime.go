2bf7c0e3c2a791c4c28b93874be7908b675577fe
Paveen Kumar <paveenkumar@accuknox.com>
2025-09-23 15:30:41 +0530

    chore(image-scan): Remove Kubearmor dependencies from image-scan
    Signed-off-by: Paveen Kumar <paveenkumar@accuknox.com>

--- pkg/imagescan/detectRuntime.go
+++ pkg/imagescan/detectRuntime.go
@@ -2,9 +2,27 @@ package imagescan
 
 import (
 	"os"
-
-	"github.com/kubearmor/KubeArmor/KubeArmor/common"
 )
+
+// referred from https://github.com/kubearmor/KubeArmor/blob/v1.6.3/KubeArmor/common/common.go#L428-L444
+// containerRuntimeSocketMap Structure
+var containerRuntimeSocketMap = map[string][]string{
+	"docker": {
+		"/var/run/docker.sock",
+		"/run/docker.sock",
+	},
+	"containerd": {
+		"/var/snap/microk8s/common/run/containerd.sock",
+		"/run/k3s/containerd/containerd.sock",
+		"/run/containerd/containerd.sock",
+		"/var/run/containerd/containerd.sock",
+		"/run/dockershim.sock",
+	},
+	"cri-o": {
+		"/var/run/crio/crio.sock",
+		"/run/crio/crio.sock",
+	},
+}
 
 func DiscoverRuntime(pathPrefix string, k8sRuntime string) (string, []string, bool) {
 	runtime, criPath := detectRuntimeViaMap(pathPrefix, k8sRuntime)
@@ -15,7 +33,7 @@ func DiscoverRuntime(pathPrefix string, k8sRuntime string) (string, []string, bo
 func detectRuntimeViaMap(pathPrefix string, runtime string) (string, []string) {
 	var sockPaths []string
 	if runtime != "" {
-		for _, path := range common.ContainerRuntimeSocketMap[runtime] {
+		for _, path := range containerRuntimeSocketMap[runtime] {
 			if _, err := os.Stat(pathPrefix + path); err == nil || os.IsPermission(err) {
 				if runtime == "docker" {
 					path = "unix://" + path
